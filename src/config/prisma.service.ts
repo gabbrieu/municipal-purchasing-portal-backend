a32d7cb96b85edcb5d7dffe182e606ed4b404df2
gabbrieu <gabrielhmendes@hotmail.com>
2024-06-29 13:40:09 -0300

    feat: add possibility to see generated sql by prisma

--- src/config/prisma.service.ts
+++ src/config/prisma.service.ts
@@ -1,9 +1,34 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+    extends PrismaClient<Prisma.PrismaClientOptions, 'query' | 'beforeExit'>
+    implements OnModuleInit
+{
+    // constructor() {
+    //     super({
+    //         log: [
+    //             {
+    //                 emit: 'event',
+    //                 level: 'query',
+    //             },
+    //         ],
+    //     });
+    // }
+
     async onModuleInit(): Promise<void> {
+        // this.$on('query', (event: Prisma.QueryEvent) => {
+        //     console.log('Query: ' + event.query + '\nParams: ' + event.params);
+        //     console.log('Duration: ' + event.duration + 'ms');
+        // });
+
         await this.$connect();
+    }
+
+    async enableShutdownHooks(app: INestApplication) {
+        this.$on('beforeExit', async () => {
+            await app.close();
+        });
     }
 }
