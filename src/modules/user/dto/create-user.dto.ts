041fe7cf0d080d7201d4de7c5891be58cd230231
gabbrieu <gabrielhmendes@hotmail.com>
2024-06-29 13:41:18 -0300

    fix: add decorators to restrict sent body to create user endpoint

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,32 +1,42 @@
 import { $Enums } from '@prisma/client';
 import {
     IsDateString,
+    IsEmail,
     IsEnum,
     IsNumber,
     IsOptional,
     IsString,
+    Length,
+    MaxLength,
 } from 'class-validator';
 
 export class CreateUserDto {
+    @MaxLength(255)
     @IsString()
     name: string;
 
+    @MaxLength(80)
+    @IsEmail()
     @IsString()
     email: string;
 
     @IsString()
+    @MaxLength(300)
     password: string;
 
     @IsDateString()
     birthDate: string;
 
+    @Length(11, 11)
     @IsString()
     cpf: string;
 
+    @Length(11, 11)
     @IsString()
     cellphone: string;
 
     @IsOptional()
+    @Length(10, 10)
     @IsString()
     telephone?: string;
 
@@ -37,24 +47,30 @@ export class CreateUserDto {
     race: $Enums.Race;
 
     @IsString()
+    @MaxLength(255)
     address: string;
 
     @IsNumber()
     addressNumber: number;
 
     @IsOptional()
+    @MaxLength(255)
     @IsString()
     complement?: string;
 
     @IsString()
+    @MaxLength(100)
     city: string;
 
+    @Length(2, 2)
     @IsString()
     state: string;
 
+    @Length(8, 8)
     @IsString()
     postalCode: string;
 
+    @MaxLength(30)
     @IsString()
     username: string;
 
